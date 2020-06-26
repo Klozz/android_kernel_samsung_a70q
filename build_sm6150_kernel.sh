ac5044adaf5d69b7a9e7167c9af3e4f835d59e15
klozz <klozz@vivaldi.net>
2020-06-26 02:17:17 +0000

    create defconfig: disable knox feature and some samsung protection remove avb and verify
    Signed-off-by: klozz <klozz@vivaldi.net>

--- build_sm6150_kernel.sh
+++ build_sm6150_kernel.sh
@@ -15,7 +15,7 @@ fi
 
 MODEL=${BUILD_COMMAND%%_*}
 
-        
+
 echo PRODUCT_NAME=$PRODUCT_NAME
 echo MODEL=$MODEL
 echo DMVERITY_DEFCONFIG=$DMVERITY_DEFCONFIG
@@ -27,10 +27,10 @@ BUILD_ROOT_DIR=$BUILD_KERNEL_DIR/../../..
 BUILD_KERNEL_OUT_DIR=$PRODUCT_OUT/obj/KERNEL_OBJ
 
 SECURE_SCRIPT=$BUILD_ROOT_DIR/buildscript/tools/signclient.jar
-BUILD_CROSS_COMPILE=$BUILD_ROOT_DIR/android/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$BUILD_ROOT_DIR/android/vendor/qcom/proprietary/llvm-arm-toolchain-ship/6.0/bin/clang
+BUILD_CROSS_COMPILE=/home/klozz/samsung/kernel/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=/home/klozz/samsung/kernel/toolchain/llvm-arm-toolchain-ship/8.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$BUILD_KERNEL_DIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y" 
+KERNEL_MAKE_ENV="DTC_EXT=$BUILD_KERNEL_DIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
@@ -46,11 +46,13 @@ CARRIER=${TEMP##*_}
 
 VARIANT=${CARRIER}
 PROJECT_NAME=${VARIANT}
-if [ "${REGION}" == "${CARRIER}" ]; then
-	VARIANT_DEFCONFIG=sm6150_sec_${MODEL}_${CARRIER}_defconfig
-else
-	VARIANT_DEFCONFIG=sm6150_sec_${MODEL}_${REGION}_${CARRIER}_defconfig
-fi
+#if [ "${REGION}" == "${CARRIER}" ]; then
+#	VARIANT_DEFCONFIG=sm6150_sec_${MODEL}_${CARRIER}_defconfig
+#else
+#	VARIANT_DEFCONFIG=sm6150_sec_${MODEL}_${REGION}_${CARRIER}_defconfig
+#fi
+
+VARIANT_DEFCONFIG=sm6150_sec_a70s_swa_open_defconfig
 
 BUILD_CONF_PATH=$BUILD_ROOT_DIR/buildscript/build_conf/${MODEL}
 BUILD_CONF=${BUILD_CONF_PATH}/common_build_conf.${MODEL}
@@ -84,15 +86,15 @@ FUNC_BUILD_KERNEL()
         fi
 
 
-    chmod u+w $PRODUCT_OUT/mkbootimg_ver_args.txt     
-    echo '--cmdline "console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000 boot_cpus=0-3 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.selinux=permissive service_locator.enable=1" --base 0x00000000 --pagesize 4096 --os_version 8.1.0 --os_patch_level 2018-04-01 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 '   >  $PRODUCT_OUT/mkbootimg_ver_args.txt   
-    
+    chmod u+w $PRODUCT_OUT/mkbootimg_ver_args.txt
+    echo '--cmdline "console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000 boot_cpus=0-3 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.selinux=permissive service_locator.enable=1" --base 0x00000000 --pagesize 4096 --os_version 8.1.0 --os_patch_level 2018-04-01 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 '   >  $PRODUCT_OUT/mkbootimg_ver_args.txt
+
     echo ----------------------------------------------
     echo info $PRODUCT_OUT/mkbootimg_ver_args.txt
     echo ----------------------------------------------
     cat  $PRODUCT_OUT/mkbootimg_ver_args.txt
     echo ----------------------------------------------
-    
+
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER $KERNEL_MAKE_ENV ARCH=arm64 \
 			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
 			REAL_CC=$KERNEL_LLVM_BIN \
@@ -111,13 +113,13 @@ FUNC_BUILD_KERNEL()
 			CLANG_TRIPLE=$CLANG_TRIPLE  || exit -1
 
 #	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER $KERNEL_MAKE_ENV ARCH=arm64 \
-#			CROSS_COMPILE=$BUILD_CROSS_COMPILE dtbs || exit -1            
+#			CROSS_COMPILE=$BUILD_CROSS_COMPILE dtbs || exit -1
 	$BUILD_KERNEL_DIR/tools/mkdtimg create $PRODUCT_OUT/dtbo.img --page_size=4096 $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/dts/samsung/sm6150-sec-${MODEL}-${REGION}-overlay*.dtbo
 
     rsync -cv $KERNEL_ZIMG $PRODUCT_OUT/kernel
-    
+
     ls -al $PRODUCT_OUT/kernel
-    
+
 	echo ""
 	echo "================================="
 	echo "END   : FUNC_BUILD_KERNEL"
